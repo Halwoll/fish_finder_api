974735a7b6c4894f846e78f0a646e10e5c56ebaa
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-12-08 16:06:51 +0000

    added basic data store

---
+++ Models/Fish.cs
@@ -0,0 +1,8 @@
+namespace FishIdentifier.Models;
+
+public class Fish
+{
+  public int Id { get; set; }
+  public string? Name { get; set; }
+  public bool FishFor {get; set; }
+}
---
+++ Services/FishService.cs
@@ -0,0 +1,40 @@
+using FishIdentifier.Models;
+
+namespace FishIdentifier.Services;
+
+public static class FishService
+{
+  static List<Fish> Fishes { get; }
+  static int nextId = 3;
+  static FishService()
+  {
+    Fishes = new List<Fish>
+    {
+      new Fish { Id = 1, Name = "Trout", FishFor = false },
+      new Fish { Id = 1, Name = "Grayling", FishFor = true }
+    };
+  }
+  public static List<Fish> GetAll() => Fishes;
+  public static Fish? Get(int id) => Fishes.FirstOrDefault(f => f.Id == id);
+  public static void Add(Fish fish)
+  {
+    fish.Id = nextId++;
+    Fishes.Add(fish);
+  }
+  public static void Delete(int id)
+  {
+    var fish = Get(id);
+    if(fish is null)
+      return;
+    
+    Fishes.Remove(fish);
+  }
+  public static void Update(Fish fish)
+  {
+    var index = Fishes.FindIndex(f => f.Id == fish.Id);
+    if (index == -1)
+      return;
+
+    Fishes[index] = fish;
+  }
+}
